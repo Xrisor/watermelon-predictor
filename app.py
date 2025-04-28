27d3df3ab0c3097af311a1d8e7b7bfb699b700cb
Xrisor <syahminazar@gmail.com>
2025-04-28 17:01:07 +0800

    app.py

--- app.py
+++ app.py
@@ -82,4 +82,6 @@ def predict():
 if __name__ == '__main__':
     if not os.path.exists('uploads'):
         os.makedirs('uploads')
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # 👈 Important line
+    app.run(host='0.0.0.0', port=port)
+
